ebd09d0bb79c3ba099f046786087c920ffe59d68
cannelljack <cannell.jack@gmail.com>
2017-09-17 23:20:53 -0500

    changed title to curlew's game builder

--- AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/GameBuilderApplication.java
+++ AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/GameBuilderApplication.java
@@ -16,7 +16,7 @@ public class GameBuilderApplication extends Application {
 	    
         Scene scene = new Scene(root);
     
-        primaryStage.setTitle("<YOUR TEAM>'s Game Builder");
+        primaryStage.setTitle("Curlew's Game Builder");
         primaryStage.setScene(scene);
         primaryStage.show();
 	}
