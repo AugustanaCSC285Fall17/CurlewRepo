5525999649d060169e2aa7178336381168d237fc
maxmccomb <maxwellmccomb16@augustana.edu>
2017-09-19 14:50:21 -0500

    added changes from class, and added a working remove slide methto game data class that updates option indicies

--- adventure-game/core/src/edu/augustana/csc285/game/datamodel/GameData.java
+++ adventure-game/core/src/edu/augustana/csc285/game/datamodel/GameData.java
@@ -7,35 +7,56 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
- * This class will represent all of the data needed to 
- * load/save an adventure game.
+ * This class will represent all of the data needed to load/save an adventure
+ * game.
  */
 public class GameData {
 	private List<Slide> slides;
-	
+
 	public GameData() { // needed for GSon
 		slides = new ArrayList<Slide>();
 	}
 
-	public List<Slide> getSlides() {
-		return slides;
+	public Slide getSlide(int index) {
+		return slides.get(index);
 	}
 
-	public void setSlides(List<Slide> slides) {
-		this.slides = slides;
+	public void addSlide(Slide slide) {
+		slides.add(slide);
 	}
-	
+
+	public void removeSlide(int index) {
+		
+		//loops through every slide
+		for (int currentSlide = 0; currentSlide < slides.size(); currentSlide++) {
+			List<ActionChoice> choices = slides.get(currentSlide).getActionChoices();
+			//loops through every choice
+			for (int currentActionChoice = 0; currentActionChoice < choices.size(); currentActionChoice++) {
+
+				// for every action choice if the destination slide index is
+				// greater than the remove slide index than the destination
+				// slide index is reduced by 1
+				if (choices.get(currentActionChoice).getDestinationSlideIndex() > index) {
+					choices.get(currentActionChoice)
+							.setDestinationSlideIndex(choices.get(currentActionChoice).getDestinationSlideIndex() - 1);
+				}
+			}
+		}
+		slides.remove(index);
+	}
+
 	/**
-	 * @return a serialized JSON-format string that represents this GameData object
+	 * @return a serialized JSON-format string that represents this GameData
+	 *         object
 	 */
 	public String toJSON() {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		return gson.toJson(this);
 	}
-	
+
 	/**
-	 * @return a GameData object, which is created from deserializing 
-	 *         the JSON data provided. 
+	 * @return a GameData object, which is created from deserializing the JSON
+	 *         data provided.
 	 */
 	public static GameData fromJSON(String jsonData) {
 		Gson gson = new GsonBuilder().create();
