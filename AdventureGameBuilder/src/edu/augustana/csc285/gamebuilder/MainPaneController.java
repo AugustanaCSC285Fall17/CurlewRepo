5d1310a106ac2fa5aa223377c6504eaea62a047c
maxmccomb <maxwellmccomb16@augustana.edu>
2017-09-20 14:13:36 -0500

    Added a see all slide message pop-up

--- AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/MainPaneController.java
+++ AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/MainPaneController.java
@@ -12,51 +12,64 @@ import edu.augustana.csc285.game.datamodel.*;
 
 public class MainPaneController {
 
-	@FXML private TextField showSlideAtTextField;
-	@FXML private Button addSlideButton;
-	@FXML private Button showSlideAtButton;
+	@FXML
+	private TextField showSlideAtTextField;
+	@FXML
+	private Button addSlideButton;
+	@FXML
+	private Button showSlideListButton;
 	private GameData data = new GameData();
-	private int slideAtTextIndex = 0; //Note Change this later, will cause problems if list is empty
-	
+	private int slideAtTextIndex = 0; // Note Change this later, will cause
+										// problems if list is empty
+
 	// JavaFX initialize method, called after this Pane is created.
 	@FXML
 	private void initialize() {
 		// Slide slide = new Slide();
 	}
-	
+
 	@FXML
-	private void handleShowSlideAtTextField(){
-		try{
-		slideAtTextIndex = Integer.parseInt(showSlideAtTextField.getText());
-		if(slideAtTextIndex<0||slideAtTextIndex>data.getSlideListSize()-1){
-			new Alert(AlertType.ERROR,"There is no slide at this index").showAndWait();
-		}else{
-		new Alert(AlertType.INFORMATION,data.getSlide(slideAtTextIndex).toString()).showAndWait();
-		}
+	private void handleShowSlideAtTextField() {
+		try {
+			slideAtTextIndex = Integer.parseInt(showSlideAtTextField.getText());
+			if (slideAtTextIndex < 0 || slideAtTextIndex > data.getSlideListSize() - 1) {
+				new Alert(AlertType.ERROR, "There is no slide at this index").showAndWait();
+			} else {
+				new Alert(AlertType.INFORMATION, data.getSlide(slideAtTextIndex).toString()).showAndWait();
+			}
 		} catch (NumberFormatException e) {
-			new Alert(AlertType.ERROR,"This was not an int.").showAndWait();
+			new Alert(AlertType.ERROR, "This was not an int.").showAndWait();
 		}
-		
+
 	}
-	
+
 	@FXML
 	private void handleAddSlideButton() {
 		data.addSlide(new Slide());
 	}
+
 	@FXML
-	private void handleShowSlideAtButton() {
-	//	new Alert(AlertType.INFORMATION, )
+	private void handleShowSlideListButton() {
+		String s = "";
+		if (data.getSlideListSize() == 0) {
+			new Alert(AlertType.INFORMATION, "There are no slides in the list").showAndWait();
+		} else {
+			for (int i = 0; i < data.getSlideListSize(); i++) {
+				s += "Slide " + i + " is " + data.getSlide(i).getTitle() + "\n";
+			}
+			new Alert(AlertType.INFORMATION, s).showAndWait();
+		}
 	}
 
 	@FXML
 	private void handleMenuFileClose() {
-		// TODO: eventually offer option to save before closing? 
+		// TODO: eventually offer option to save before closing?
 		Platform.exit();
 	}
 
 	@FXML
 	private void handleMenuHelpAbout() {
-		new Alert(AlertType.INFORMATION,"Placeholder for about screen").showAndWait();
+		new Alert(AlertType.INFORMATION, "Placeholder for about screen").showAndWait();
 	}
 
 }
