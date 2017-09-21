9bdf478768f59c38fd672871700b06d3ae5fc65e
maxmccomb <maxwellmccomb16@augustana.edu>
2017-09-21 16:21:06 -0500

    Added new text fields and labels to game builder gui. Also created a SlideEditor class

--- AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/MainPaneController.java
+++ AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/MainPaneController.java
@@ -7,11 +7,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import edu.augustana.csc285.game.datamodel.*;
 
 public class MainPaneController {
-
 
 	@FXML
 	private TextField showSlideAtTextField;
@@ -20,8 +20,14 @@ public class MainPaneController {
 	@FXML
 	private Button showSlideListButton;
 	private GameData data = new GameData();
-	private int slideAtTextIndex = 0; // Note Change this later, will cause
-										// problems if list is empty
+	private int slideAtTextIndex = 0;
+
+	// Room Editor Fields
+	private SlideEditor re = new SlideEditor(data);
+	@FXML
+	private TextField selectSlideNumberTextField;
+	@FXML
+	private Label currentSlideLabel;
 
 	// JavaFX initialize method, called after this Pane is created.
 	@FXML
@@ -29,19 +35,32 @@ public class MainPaneController {
 		// Slide slide = new Slide();
 	}
 
-	@FXML
-	private void handleShowSlideAtTextField() {
+	private boolean isInputInt(String s) {
 		try {
-			slideAtTextIndex = Integer.parseInt(showSlideAtTextField.getText());
-			if (slideAtTextIndex < 0 || slideAtTextIndex > data.getSlideListSize() - 1) {
-				new Alert(AlertType.ERROR, "There is no slide at this index").showAndWait();
-			} else {
-				new Alert(AlertType.INFORMATION, data.getSlide(slideAtTextIndex).toString()).showAndWait();
-			}
+			int i = Integer.parseInt(s);
+			return true;
 		} catch (NumberFormatException e) {
 			new Alert(AlertType.ERROR, "This was not an int.").showAndWait();
+			return false;
 		}
+	}
 
+	private boolean isIndexASlide(int index) {
+		if (index < 0 || index > data.getSlideListSize() - 1) {
+			new Alert(AlertType.ERROR, "There is no slide at this index").showAndWait();
+			return false;
+		}
+		return true;
+	}
+
+	@FXML
+	private void handleShowSlideAtTextField() {
+		if (isInputInt(showSlideAtTextField.getText())) {
+			slideAtTextIndex = Integer.parseInt(showSlideAtTextField.getText());
+			if (isIndexASlide(slideAtTextIndex)) {
+				new Alert(AlertType.INFORMATION, data.getSlide(slideAtTextIndex).toString()).showAndWait();
+			}
+		}
 	}
 
 	@FXML
@@ -72,6 +91,17 @@ public class MainPaneController {
 	@FXML
 	private void handleMenuHelpAbout() {
 		new Alert(AlertType.INFORMATION, "Placeholder for about screen").showAndWait();
+	}
+
+	@FXML
+	private void handleSelectSlideNumberTextField() {
+		if (isInputInt(selectSlideNumberTextField.getText())) {
+			int index = Integer.parseInt(selectSlideNumberTextField.getText());
+			if (isIndexASlide(index)) {
+				re.setCurrentSlide(index);
+				currentSlideLabel.setText(Integer.toString(index));
+			}
+		}
 	}
 
 }
---
+++ AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/SlideEditor.java
@@ -0,0 +1,27 @@
+package edu.augustana.csc285.gamebuilder;
+import edu.augustana.csc285.game.datamodel.*;
+
+public class SlideEditor {
+
+	private int currentSlideIndex;
+	private GameData data;
+	
+	
+	
+	public SlideEditor(GameData data) {
+		this.currentSlideIndex = -1;
+		this.data = data;
+	}
+
+	public int getCurrentSlide() {
+		return currentSlideIndex;
+	}
+
+	public void setCurrentSlide(int currentRoom) {
+		this.currentSlideIndex = currentRoom;
+	}
+	
+	public void setTitle(String title){
+		
+	}
+}
