8d9b3ffe65c107404c5ad4698b7ec478ecbd2961
maxmccomb <maxwellmccomb16@augustana.edu>
2017-11-01 20:41:51 -0500

    Added javadoc to genderchange effect

--- adventure-game/core/src/edu/augustana/csc285/game/datamodel/GenderChangeEffect.java
+++ adventure-game/core/src/edu/augustana/csc285/game/datamodel/GenderChangeEffect.java
@@ -4,35 +4,47 @@ public class GenderChangeEffect implements Effect {
 
 	private Gender newGender;
 	
-	public GenderChangeEffect() {
-	}
-	
+	/**
+	 * creates a new gender by setting an inputed gender 
+	 * @param newGender the gender that the user would like to 
+	 */
 	public GenderChangeEffect(Gender newGender) {
 		this.newGender = newGender;
 	}
-	
+	/**
+	 * puts the effect into effect
+	 */
 	@Override
 	public void applyEffect(Player p) {
 		p.setGender(newGender);
 	}
 
+	/**
+	 * need for abstract methods in other effect classes
+	 */
 	@Override
-	public void setEffectSize(int size) {
-		// need for abstract methods in other effect classes
-	}
-	
-	public void setEffectName(String name){
-		// need for abstract methods in other effect classes
-	}
-	
+	public void setEffectSize(int size) {}
+	/**
+	 * need for abstract methods in other effect classes
+	 */
+	public void setEffectName(String name){}
+	/**
+	 * getter method for the effect size. always returns 0
+	 * needed for abstract methods in other effect classes
+	 */
 	public int getEffectSize(){
 		return 0;
 	}
-	
+	/**
+	 * used to display a pop up of the gender change
+	 */
 	public String printEffectInfo(){
 		return "Gender change to: " + newGender;
 	}
 	
+	/**
+	 * to string of the gender effect returns just the gender object
+	 */
 	@Override
 	public String toString() {
 		return newGender + "";
