97d88c2a58c6f775df9a02109b8db30f1c01d841
maxmccomb <maxwellmccomb16@augustana.edu>
2017-11-01 19:37:58 -0500

    added javadoc to action choice

--- adventure-game/core/src/edu/augustana/csc285/game/datamodel/ActionChoice.java
+++ adventure-game/core/src/edu/augustana/csc285/game/datamodel/ActionChoice.java
@@ -97,17 +97,24 @@ public class ActionChoice {// implements Feasible
 	}
 
 	/**
-	 * remove effect
+	 * remove effect with an index
 	 * @param index
 	 */
 	public void removeEffect(int index) {
 		effectList.remove(index);
 	}
-
+	/**
+	 * removes effect with a given effect object
+	 * @param e effect that is to be removed
+	 */
 	public void removeEffect(Effect e) {
 		effectList.remove(e);
 	}
-	
+	/**
+	 *  removes a condition given an index and an int that distinguishes between feasibility or visibility
+	 * @param index index of the desired slide
+	 * @param visOrFeas checks to see if the condition is a feasibility condition or a visibility condition
+	 */
 	public void removeCondition(int index, int visOrFeas){
 		if(visOrFeas== FEASIBILITY){
 			feasibilityCond.remove(index);
@@ -115,7 +122,11 @@ public class ActionChoice {// implements Feasible
 			this.visibilityCond.remove(index);
 		}
 	}
-
+	/**
+	 * removes a condition given a condition and an int that distinguishes between feasibility or visibility
+	 * @param condition the object that is to be removed
+	 * @param visOrFeas checks to see if the condition is a feasibility condition or a visibility condition
+	 */
 	public void removeCondition(Condition condition, int visOrFeas){
 		if(visOrFeas== FEASIBILITY){
 			feasibilityCond.remove(condition);
@@ -123,29 +134,41 @@ public class ActionChoice {// implements Feasible
 			this.visibilityCond.remove(condition);
 		}
 	}
-	// changes the effect name
+	/**
+	 *  changes the effect name
+	 * @param newEffectName the new name of the effect
+	 * @param index of the effect
+	 */
 	public void setEffectName(String newEffectName, int index) {
 		effectList.get(index).setEffectName(newEffectName);
 	}
 
-	// changes the effect size
+	/**
+	 * changes the effect size
+	 * @param newEffectSize the new effect size
+	 * @param index of the effect that is being edited
+	 */
 	public void setEffectSize(int newEffectSize, int index) {
 		effectList.get(index).setEffectSize(newEffectSize);
 	}
-
-	// check if the option will be fersible
-	public void isFeasible(ArrayList<Feasible> feasibilityReq) {
-
-	}
-	
+	/**
+	 * 
+	 * @return the list of feasibility conditions
+	 */
 	public ArrayList<Condition> getFeasibilityCond() {
 		return feasibilityCond;
 	}
-
+	/**
+	 * 
+	 * @return the list of visibility conditions
+	 */
 	public ArrayList<Condition> getVisibilityCond() {
 		return visibilityCond;
 	}
-
+	/**
+	 * used to display information about the effect that we do not want displayed in the toString
+	 * @return s the string of string info
+	 */
 	public String getEffectsString() {
 		String s = "You gained:\n";
 		for (Effect effect : effectList) {
@@ -161,7 +184,9 @@ public class ActionChoice {// implements Feasible
 		
 		return s;
 	}
-	
+	/**
+	 * returns the string of action choice info to be displayed in the preview pane
+	 */
 	public String toString() {
 		String s = "choiceText: " + choiceText + "\ndestinationSlideIndex: " + destinationSlideIndex;
 		for (Effect effect : effectList) {
@@ -177,15 +202,24 @@ public class ActionChoice {// implements Feasible
 		}
 		return s;
 	}
-
+	/**
+	 * 
+	 * @return the rejection text
+	 */
 	public String getRejText() {
 		return rejText;
 	}
-
+	/**
+	 * 
+	 * @param rejText the new rejection text
+	 */
 	public void setRejText(String rejText) {
 		this.rejText = rejText;
 	}
-
+	/**
+	 * checks to see if an action choice has a gender effect
+	 * @return true if the action choice has a gender effect and false otherwise
+	 */
 	public boolean hasGenderEffect() {
 		for (int i = 0; i < effectList.size(); i++) {
 			if (effectList.get(i) instanceof GenderChangeEffect) {
@@ -194,7 +228,11 @@ public class ActionChoice {// implements Feasible
 		}
 		return false;
 	}
-
+	/**
+	 *  checks to see if an action choice has a item effect
+	 * @param itemChoice the specified item that we are checking if we have an effect for
+	 * @return true if the action choice has a item effect for the specified item and false otherwise
+	 */
 	public boolean hasItemEffect(Item itemChoice) {
 		for (int i = 0; i < effectList.size(); i++) {
 			if (effectList.get(i) instanceof ItemEffect) {
@@ -206,7 +244,11 @@ public class ActionChoice {// implements Feasible
 		}
 		return false;
 	}
-	
+	/**
+	 *  checks to see if an itemCondition is feasible 
+	 * @param item the specified item
+	 * @return true if it is feasible and false otherwise
+	 */
 	public boolean hasItemConditonF(Item item){
 		for (int i = 0; i < feasibilityCond.size(); i++) {
 			if (feasibilityCond.get(i) instanceof ItemCondition) {
@@ -218,6 +260,11 @@ public class ActionChoice {// implements Feasible
 		}
 		return false;
 	}
+	/**
+	 *  checks to see if an itemCondition is visible 
+	 * @param item the specified item
+	 * @return true if it is visible and false otherwise
+	 */
 	public boolean hasItemConditonV(Item item){
 		for (int i = 0; i < visibilityCond.size(); i++) {
 			if (visibilityCond.get(i) instanceof ItemCondition) {
@@ -230,7 +277,10 @@ public class ActionChoice {// implements Feasible
 		return false;
 	}
 
-
+	/**
+	 * checks to see if an action choice has a name change effect
+	 * @return true if the action choice has a name change effect and false otherwise
+	 */
 	public boolean hasNameChangeEffect() {
 		for (int i = 0; i < effectList.size(); i++) {
 			if (effectList.get(i) instanceof NameChangeEffect) {
