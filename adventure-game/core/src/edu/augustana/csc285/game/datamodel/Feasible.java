a6c785e3682af8c519a09a61566cd0fd3274ab0c
Jia <zijianjia15@augustana.edu>
2017-11-01 19:41:31 -0500

    add new JUnit Test on Player and Slide

--- adventure-game/core/src/edu/augustana/csc285/game/datamodel/Feasible.java
+++ adventure-game/core/src/edu/augustana/csc285/game/datamodel/Feasible.java
@@ -1,30 +1,47 @@
 package edu.augustana.csc285.game.datamodel;
 
 public interface Feasible {
-	public String feasibilityTypes ="";
+	public String feasibilityTypes = "";
 	public boolean isFeasible = false;
 	public boolean haveItem = false;
-	
-	//abstract method for get the requirement of the feasibility
-		public abstract void getReq (int requirement);
-		
+
+	// abstract method for get the requirement of the feasibility
+	public abstract void getReq(int requirement);
+
 	// Check if the option is feasible
-		public abstract boolean isFeasible(String feasiblityType);
-		
+	public abstract boolean isFeasible(String feasiblityType);
+
 	// get the FeasibilityType information and set it
-		public String getFeasibilityType();
-		
-	//
-		public boolean getIsFeasible();
-		
-		public abstract void setFeasibilityType(String feraibilityType);
-		
-		public abstract void setFeasiableStat(boolean isFeasible);
-		
-		public abstract boolean checkInventory();
-		
-		
-		
-		
+	public String getFeasibilityType();
+
+	/**
+	 * return if the option is feasible
+	 * 
+	 * @return true or false
+	 */
+	public boolean getIsFeasible();
+
+	/**
+	 * set the feasibility type for an option
+	 * 
+	 * @param feasibilityType
+	 *            String that contain the feasibility type
+	 */
+	public abstract void setFeasibilityType(String feraibilityType);
+
+	/**
+	 * set the feasibility status for the option
+	 * 
+	 * @param isFeasible
+	 *            true or false
+	 */
+	public abstract void setFeasiableStat(boolean isFeasible);
+
+	/**
+	 * check the inventory
+	 * 
+	 * @return a boolean that state if the inventory has a feasibility option
+	 */
+	public abstract boolean checkInventory();
 
 }
