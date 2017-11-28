987bfbc8ebd180bbef79b9f9e695c5bc1ca8ae92
Cannell <jackcannell15@augustana.edu>
2017-11-28 15:00:50 -0600

    modified all screens in atempt to fix scroll bug

--- adventure-game/core/src/edu/augustana/csc285/game/MainMenuScreen.java
+++ adventure-game/core/src/edu/augustana/csc285/game/MainMenuScreen.java
@@ -190,6 +190,7 @@ public class MainMenuScreen implements Screen {
 
 	@Override
 	public void resize(int width, int height) {
+		game.stage.getViewport().update(width, height, true);
 	}
 
 	@Override
